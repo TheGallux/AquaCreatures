788b3428f4c2b84aaab78d63208f0f4bf3e0738e
Clement CHOLLET <clement.chollet@epita.fr>
2026-08-18 20:41:49 +0200

    chore: initial commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * @brief AquaCreatures entry point
+ * @return Exit code:
+ *    - 0 on success
+ *    - non zero on failure.
+ */
+int main(void) {
+
+    printf("Hello World!\n");
+
+    return 0;
+}
